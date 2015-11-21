8f4c8f734d3b5435ffcb90171fc26bcb671fbc01
Mrfuzzy <league@iMac.attlocal.net>
2015-11-20 18:22:52 -0800

    whack a mole continuation

--- src/hello/WhackAMole.java
+++ src/hello/WhackAMole.java
@@ -9,6 +9,7 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
@@ -18,16 +19,21 @@ public class WhackAMole implements ActionListener {
 
 	JFrame frame = new JFrame("For Fame Glory and Guts!");
 	JPanel panel = new JPanel();
+	int misses = 0;
+	int clicks = 0;
+	int points = 0;
 
 	public void createUI() {
+		frame = new JFrame("For Fame Glory and Guts!");
+		panel = new JPanel();
 		int randNum = new Random().nextInt(20);
-		frame.setVisible(true);
 		frame.setSize(300, 300);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation((dim.width / 2) - (frame.getSize().width / 2), (dim.height / 2) - (frame.getSize().height / 2));
 		frame.add(panel);
 		drawButton(randNum);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 	public void drawButton(int num) {
@@ -52,17 +58,51 @@ public class WhackAMole implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton) e.getSource();
-		if(buttonPressed.getText().equals("Mole!")){
+		if (buttonPressed.getText().equals("Mole!")) {
 			System.out.println("Bang!");
 			speak("guts");
 			frame.dispose();
-			drawButton(24);
+			createUI();
+			points += 1;
+			done();
+		} else {
+			System.out.println("Woosh");
+			insult();
+			reload();
+			done();
+		}
+	}
+
+	public void reload() {
+		if (misses == 4) {
+			frame.dispose();
+			misses = 0;
+			createUI();
 		}
 		else{
-			System.out.println("Woosh");
+			misses+=1;
+		}
+	}
+
+	public void done() {
+		if (clicks == 10) {
+			frame.dispose();
+			JOptionPane.showMessageDialog(null, "Your score is " + points);
+		}else{
+			clicks++;
+		}
+	}
+
+	public void insult() {
+		if (misses == 1) {
 			speak("Dork Face");
-			drawButton(24);
-			
+		}
+		if (misses == 2) {
+			speak("noob");
+		}
+		if (misses == 3) {
+			speak("Mor ran");
+			misses++;
 		}
 	}
 }
